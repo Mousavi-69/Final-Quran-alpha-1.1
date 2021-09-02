86ea74999489e1c0ec49a8436d197d5dfc79816f
Mousavi-69 <86127641+Mousavi-69@users.noreply.github.com>
2021-09-02 13:06:21 +0430

    Add main.scss & config sass - & style in <body>

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class="container"> 
 test
 سلام
 <!-- <router-link></router-link>
@@ -17,13 +17,12 @@ export default defineComponent({
 })
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+ <style lang="scss" scoped>
+ 
+.container{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: red;
+ 
 }
 </style>
+
