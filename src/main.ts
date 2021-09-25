aacfea7e0c805f586fd8ad666ffdeb6446970a91
Mousavi-69 <86127641+Mousavi-69@users.noreply.github.com>
2021-09-25 14:14:51 +0330

    Add FontAwesome pakage

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './Routing'
-import store from './Store'
+import store from './Store' 
 import  './styles/sass/main.scss'
-
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import{library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
 createApp(App)
+.component('fa',FontAwesomeIcon)
 .use(router)
 .use(store)
 .mount('#app')
